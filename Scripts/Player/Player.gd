4baca63b23405e706be1e511af1d3c01f882e60b
quy1099ht <quy1099ht@gmail.com>
2024-03-03 17:06:23 +0700

    Fix turn left and right not triggering animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -31,10 +31,10 @@ func _process(delta):
 		attack()
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("Left"):
+	if Input.is_action_just_pressed("ui_left"):
 		sprite.scale.x = abs(sprite.scale.x) * -1
 		$AttackArea.scale.x = abs($AttackArea.scale.x) * -1
-	if Input.is_action_just_pressed("Right"):
+	if Input.is_action_just_pressed("ui_right"):
 		sprite.scale.x = abs(sprite.scale.x) 
 		$AttackArea.scale.x = abs($AttackArea.scale.x) 
 	
